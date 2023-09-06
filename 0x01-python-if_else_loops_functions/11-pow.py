ef73fa0fac5e7ae71539048f7bf8bf1518e3123a
valebuyondo <buyondovale@gmail.com>
2023-09-06 11:18:38 -0700

    11-main.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
