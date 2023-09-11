4c58bc512cc62faee67cac7e9828b23165136e08
valebuyondo <buyondovale@gmail.com>
2023-09-11 05:50:58 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+
+guillaume@ubuntu:~/0x03$ ./8-main.py
+Length: 22 - First character: A
