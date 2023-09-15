56c962ac49c5d853a67fdce8546f9ef8789713ba
valebuyondo <buyondovale@gmail.com>
2023-09-15 10:38:56 -0700

    7-main.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
-    # Check if the key exists in the dictionary
-    if key in a_dictionary:
-        # If the key exists, replace the value
-        a_dictionary[key] = value
-    else:
-        # If the key doesn't exist, add a new key-value pair
-        a_dictionary[key] = value
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
