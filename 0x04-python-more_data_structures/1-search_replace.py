502b8768db08b0e84b7b258bcaeae3d7e7106145
valebuyondo <buyondovale@gmail.com>
2023-09-15 02:37:59 -0700

    1-main.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    # Create a new list to store the result
+    result_list = []
+
+    # Iterate through the elements in my_list
+    for element in my_list:
+        # If the current element matches the search element, append the replacement element
+        if element == search:
+            result_list.append(replace)
+        else:
+            # Otherwise, append the current element as it is
+            result_list.append(element)
+
+    return result_list
+
