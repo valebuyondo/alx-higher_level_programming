6da8b5575885cdd96905d6fc04aa0686c79b0819
valebuyondo <buyondovale@gmail.com>
2023-09-15 03:16:32 -0700

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     # Use the symmetric_difference method to find elements present in only one set
     diff_set = set_1.symmetric_difference(set_2)
