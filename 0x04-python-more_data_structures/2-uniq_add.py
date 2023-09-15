12dc8738af5742ebce95b12312d052838edea6db
valebuyondo <buyondovale@gmail.com>
2023-09-15 02:55:27 -0700

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,11 +8,11 @@ def uniq_add(my_list=[]):
 
     # Iterate through the elements in my_list
     for element in my_list:
-        # Check if the element is an integer and not already in the unique_integers set
+        # Check if the element is an integer and
+        # not already in the unique_integers set
         if isinstance(element, int) and element not in unique_integers:
             # Add the unique integer to the set and add it to the sum
             unique_integers.add(element)
             sum_unique += element
 
     return sum_unique
-
