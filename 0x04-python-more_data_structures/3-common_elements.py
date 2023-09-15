3ac88129a3fa42cf5ed340734f9a83ca9c7e201f
valebuyondo <buyondovale@gmail.com>
2023-09-15 03:04:12 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,6 +2,5 @@
 def common_elements(set_1, set_2):
     # Use the intersection method to find common elements
     common_set = set_1.intersection(set_2)
-    
-    return common_set
 
+    return common_set
