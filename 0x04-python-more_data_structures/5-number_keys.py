d0151775f408c904fa89658aac2bb0cd42b14e92
valebuyondo <buyondovale@gmail.com>
2023-09-15 03:24:01 -0700

    5-main.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    # Use the len() function to 
+    # count the number of keys in the dictionary
+    count = len(a_dictionary.keys())
+
+    return count
