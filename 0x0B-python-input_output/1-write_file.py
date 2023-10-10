aad233554de9440cc9afb023a1d837a611c14796
valebuyondo <buyondovale@gmail.com>
2023-10-10 02:01:29 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Defines a file-writing function."""
 
-"""Defines a text file line-counting function."""
 
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
