a3c11bcdd1931ea5322fd943040f306a986d6b0f
valebuyondo <buyondovale@gmail.com>
2023-12-21 23:39:15 -0800

    all: '

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
-# Usage: ./4-cities_by_state.py <mysql username> \
-#                               <mysql password> \
-#                               <database name>
-import sys
+""" Script takes an argument and sql injection"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall()]
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        host="localhost",
+        port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities INNER JOIN states \
+                    ON cities.state_id=states.id")
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    db.cursor()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
+""" takes name of a state as an argument and lists all cities"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        host="localhost",
+        port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+                    INNER JOIN states ON \
+                    cities.state_id=states.id \
+                    WHERE states.name=%s", (sys.argv[4],))
+    row = cursor.fetchall()
+    sp = ""
+    ar = list(r[0] for r in row)
+    for i in row:
+        sp = sp + i[0] + ", "
+    print(sp[0: -2:])
+    cursor.close()
+    db.close()
